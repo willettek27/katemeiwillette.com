e96895b5034dbb6d1d5fc64b8936e2e36682916e
Kathryn Willette <kathrynwillette@Kathryns-MacBook-Pro.local>
2025-06-10 19:41:25 -0500

    revisions

--- main.go
+++ main.go
@@ -8,17 +8,15 @@ import (
 )
 
 func main() {
-	// Serve static files from the static directory
 	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("styles/"))))
 
 	http.HandleFunc("/", home)
+	// http.HandleFunc("/about", about)
 	http.HandleFunc("/projects", projects)
-	http.HandleFunc("/about", about)
-	http.HandleFunc("/contact", contact)
+	// http.HandleFunc("/contact", contact)
 
-	fmt.Println("🚀 Server is running on http://localhost:8080")
+	fmt.Println("Server is running on http://katemeiwillette.com")
 
-	// Start the web server on port 8080 and listen to incoming requests
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
@@ -29,17 +27,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "home.html")
 }
 
+/* func about(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.html")
+}
+*/
+
 func projects(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "projects.html")
 }
 
-func about(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.html")
-}
-
+/*
 func contact(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "contact.html")
 }
+*/
 
 func renderTemplate(w http.ResponseWriter, tmpl string) {
 
