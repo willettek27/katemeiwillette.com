2efa6a71567b2aa4f7eacb3774450c3283588b32
Kathryn Willette <kathrynwillette@Kathryns-MacBook-Pro.local>
2025-07-05 14:21:54 -0500

    revisions

--- main.go
+++ main.go
@@ -8,15 +8,16 @@ import (
 )
 
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	http.HandleFunc("/", index)
 	// http.HandleFunc("/about", about)
 	http.HandleFunc("/projects", projects)
 	http.HandleFunc("/contact", contact)
-	fmt.Println("Server is running on http://katemeiwillette.com")
+	port := ":8080"
+	fmt.Printf("Listening on %s\n", port)
 
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,15 +36,13 @@ func projects(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "projects.html")
 }
 
-
 func contact(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "contact.html")
 }
 
-
 func renderTemplate(w http.ResponseWriter, tmpl string) {
 
-	t, err := template.ParseFiles("templates/" + tmpl)
+	t, err := template.ParseFiles("html/" + tmpl)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
